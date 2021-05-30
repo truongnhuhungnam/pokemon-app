bf3bb6da619ea503b19b9052a62ff0ae3cab8d54
zoro0307 <truongnhuhungnam@gmail.com>
2021-05-30 14:55:56 +0700

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
     <div>
         <div
             v-if="pokemon"
-            class="w-3/12 m-auto bg-purple-100 mt-4 shadow-2xl flex justify-center flex-col items-center"
+            class="w-3/12 m-auto bg-purple-100 mt-4 p-4 shadow-2xl flex justify-center flex-col items-center"
         >
             <h3 class="text-2xl text-green-900 uppercase">
                 {{ pokemon.name }}
@@ -12,8 +12,14 @@
                 <img :src="pokemon.sprites.back_default" alt="" />
             </div>
             <h3 class="text-yellow-400">Type</h3>
-            <div v-for="(type, idx) in pokemon.types" :key="idx">
-                <h5 class="text-blue-900">{{ type.type.name }}</h5>
+            <div class="flex mt-4">
+                <div v-for="(type, idx) in pokemon.types" :key="idx">
+                    <h5
+                        class="text-blue-900 uppercase px-2 rounded bg-blue-50 mr-2"
+                    >
+                        {{ type.type.name }}
+                    </h5>
+                </div>
             </div>
         </div>
     </div>
